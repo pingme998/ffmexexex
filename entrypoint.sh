d9da4ec07970e6299e5b476d8c31663cfe1f06fe
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-11-23 16:00:58 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/pingme998/ffmexexex
 echo $PORT >/PORTS
+curl -L "$CONFIG_IN_URL" >$(rclone config file |grep 'rclone.conf')
 jupyter notebook --ip=0.0.0.0 --port=$(cat /PORTS) --NotebookApp.token='' --NotebookApp.password=''
